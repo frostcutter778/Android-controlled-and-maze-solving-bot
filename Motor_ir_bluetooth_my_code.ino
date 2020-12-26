5b13190aa93c7cbd440cf2686ad7bbf72609f880
frostcutter778 <63427018+frostcutter778@users.noreply.github.com>
2020-12-26 10:09:13 -0800

    Initial Commit

---
+++ Motor_ir_bluetooth_my_code.ino
@@ -0,0 +1,233 @@
+#include <QTRSensors.h>
+#define enB 10
+#define enA 9
+#define in1 2
+#define in2 3
+#define in3 4
+#define in4 5
+
+QTRSensors qtr;
+
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+boolean mode = LOW; // Low for motor , High for sensor 
+int sensorFlag=LOW; // Low for sensor calibration  , High for Not calibration 
+const byte interruptPin = 6;
+long unsigned int lastPress;
+int debounceTime= 40;
+boolean ledstate= LOW;
+unsigned int ledtime=0;
+
+
+
+void SensorCal(){
+  // configure the sensors
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, A6, A7}, SensorCount);
+  qtr.setEmitterPin(11);
+
+  delay(500);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+
+  // analogRead() takes about 0.1 ms on an AVR.
+  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
+  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
+  // Call calibrate() 400 times to make calibration take about 10 seconds.
+  for (uint16_t i = 0; i < 400; i++)
+  {
+    qtr.calibrate();
+  }
+  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+
+  // print the calibration minimum values measured when emitters were on
+  Serial.begin(9600);
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // print the calibration maximum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+  delay(1000);
+  }
+
+void SensorData(){
+  // read calibrated sensor values and obtain a measure of the line position
+  // from 0 to 5000 (for a white line, use readLineWhite() instead)
+  
+  uint16_t position = qtr.readLineBlack(sensorValues);
+
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+  Serial.println(position);
+
+  delay(250);
+  if(ledtime>6){
+    ledstate=!ledstate;
+    ledtime=0;
+    }
+  digitalWrite(LED_BUILTIN, ledstate);
+  ledtime++;
+  }
+  
+void forward(){
+  analogWrite(enA,150);
+  analogWrite(enB,150);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  }
+void back(){
+  analogWrite(enA,150);
+  analogWrite(enB,150);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  }
+void right(){
+  analogWrite(enA,150);
+  analogWrite(enB,150);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+}
+
+void left(){
+  analogWrite(enA,150);
+  analogWrite(enB,150);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  }
+  
+
+void off(){
+  analogWrite(enA,0);
+  analogWrite(enB,0);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  }
+void motorTest(){
+   forward();
+  Serial.println("F");
+  delay(1000);
+  back();
+  Serial.println("B");
+  delay(1000);
+  right();
+  Serial.println("R");
+  delay(1000);
+  left();
+  Serial.println("L");
+  delay(1000);
+  off();
+  Serial.println("stop");
+  delay(1000);
+  
+  
+  } 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  Serial.begin(9600);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), changeMode, FALLING);
+  
+
+}
+
+void loop() {
+
+  if(mode==LOW){
+    if(Serial.available()>0)
+    {
+    int inbyte= Serial.read();
+
+    switch(inbyte){
+    case '2':
+    Serial.println("L");
+    left();
+    delay(1000);
+    break;
+    case '3':
+    Serial.println("R");
+    right();
+     delay(1000);
+    break;
+    
+    case '1':
+    Serial.println("U");
+    forward();
+     delay(1000);
+    break;
+    case '4':
+    Serial.println("B");
+    back();
+     delay(1000);
+    break;
+    case '5':
+    Serial.println("D");
+    off();
+     delay(1000);
+    break; 
+    default:
+    break;      
+    }
+  }   
+    }
+  else if(mode==HIGH){
+  digitalWrite(LED_BUILTIN, LOW);     
+       if(sensorFlag==LOW){
+        Serial.println("IR Test");
+        SensorCal();
+        sensorFlag=HIGH;
+        }else{
+        SensorData();
+        }
+       }
+      }
+
+ 
+ 
+
+void changeMode(){
+  
+  if((millis()-lastPress) > debounceTime)
+  {
+    lastPress = millis();
+    if(digitalRead(interruptPin) == 0)
+    {
+       mode=!mode;
+      }
+    else if(digitalRead(interruptPin) == 1)
+    {
+      mode=mode;
+      }  
+   }
+  }
---
+++ pid-test.ino
@@ -0,0 +1,132 @@
+///Make sure to install the library
+#include <QTRSensors.h>
+QTRSensors qtr;
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+
+float Kp = 1.0; //set up the constants value
+float Ki = 0.4;
+float Kd = 0.5;
+int P;
+int I;
+int D;
+
+int lastError = 0;
+boolean onoff = false;
+
+//Increasing the maxspeed can damage the motors - at a value of 255 the 6V motors will receive 7,4 V 
+const uint8_t maxspeeda = 50;
+const uint8_t maxspeedb = 50;
+const uint8_t basespeeda = 50;
+const uint8_t basespeedb = 50;
+
+//Set up the drive motor carrier pins
+int bphase2= 2;
+int bphase = 3;
+int apwm = 9;
+int aphase = 4;
+int aphase2=5;
+int bpwm = 10;
+
+
+
+//Set up the buttons pins
+int buttoncalibrate = 6; //pin A3
+int buttonstart = 7;
+
+void setup() {
+  Serial.begin(9600);
+  //qtr.setTypeRC();
+  qtr.setTypeAnalog();
+  //Set up the sensor array pins
+  qtr.setSensorPins((const uint8_t[]){A0,A1,A2,A3,A4,A5,A6,A7}, SensorCount);
+  qtr.setEmitterPin(11);//LEDON PIN
+
+ 
+  pinMode(aphase2, OUTPUT);
+  pinMode(aphase, OUTPUT);
+  pinMode(bphase, OUTPUT);
+  pinMode(bphase2, OUTPUT);
+  pinMode(apwm,OUTPUT);
+  pinMode(bpwm,OUTPUT);
+
+
+  delay(500);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  boolean Ok = false;
+  while (Ok == false) { //the loop won't start until the robot is calibrated
+    if(digitalRead(buttoncalibrate) == HIGH) {
+      calibration(); //calibrate the robot for 10 seconds
+      Ok = true;
+    }
+  }
+  forward_brake(0,0);
+}
+
+void calibration() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  for (uint16_t i = 0; i < 400; i++)
+  {
+    qtr.calibrate();
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+  if(digitalRead(buttonstart) == HIGH) {
+    onoff =! onoff;
+    if(onoff = true) {
+      delay(1000);//a delay when the robot starts
+    }
+    else {
+      delay(50);
+    }
+  }
+  if (onoff == true) {
+    PID_control();
+  }
+  else {
+    forward_brake(0,0);
+  }
+}
+void forward_brake(int posa, int posb) {
+  //set the appropriate values for aphase and bphase so that the robot goes straight
+digitalWrite(aphase, LOW);
+  digitalWrite(aphase2, HIGH);
+  digitalWrite(bphase, HIGH);
+  digitalWrite(bphase2, LOW);
+  digitalWrite(apwm, posa);
+  digitalWrite(bpwm, posb);
+ 
+}
+void PID_control() {
+  uint16_t posit = qtr.readLineBlack(sensorValues);
+  Serial.println(sensorValues[SensorCount]);
+  Serial.print(posit);
+  int error = 3500 - posit;
+
+  P = error;
+  I = I + error;
+  D = error - lastError;
+  lastError = error;
+  int motorspeed = P*Kp + I*Ki + D*Kd;
+  
+  int motorspeeda = basespeeda + motorspeed;
+  int motorspeedb = basespeedb - motorspeed;
+  
+  if (motorspeeda > maxspeeda) {
+    motorspeeda = maxspeeda;
+  }
+  if (motorspeedb > maxspeedb) {
+    motorspeedb = maxspeedb;
+  }
+  if (motorspeeda < 0) {
+    motorspeeda = 0;
+  }
+  if (motorspeedb < 0) {
+    motorspeedb = 0;
+  } 
+  //Serial.print(motorspeeda);Serial.print(" ");Serial.println(motorspeedb);
+  forward_brake(motorspeeda, motorspeedb);
+}
